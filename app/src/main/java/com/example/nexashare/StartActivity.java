ab93c88bd2a76915640586d95ec239af4c226412
Kevinsholim06 <kipsangkevin06@gmail.com>
2023-10-23 22:11:46 +0300

    login and registration activities

---
+++ app/src/main/java/com/example/nexashare/StartActivity.java
@@ -0,0 +1,37 @@
+package com.example.nexashare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class StartActivity extends AppCompatActivity {
+    public Button register;
+    public TextView login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        register = (Button)findViewById(R.id.get_started_btn);
+        login = (TextView)findViewById(R.id.start_login_txt);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent register = new Intent(StartActivity.this,RegisterActivity.class);
+                startActivity(register);
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent login = new Intent(StartActivity.this,LoginActivity.class);
+                startActivity(login);
+            }
+        });
+    }
+}
